2887a207bcc114ec3301eddd0c004d01ad103c3b
Sagar Jaswal <sagarjaswal9102001@gmail.com>
2021-07-06 11:27:53 +0530

    plant app

--- plantapp/lib/widgets/body.dart
+++ plantapp/lib/widgets/body.dart
@@ -1,88 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 import '../constant.dart';
+import '../widgets/title_with_more_button.dart';
+import '../widgets/title_with_custom_underline.dart';
+import '../widgets/header_with_search_box.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size =
         MediaQuery.of(context).size; //it will provide us total height of screen
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2, //it will cover 20% of our total height
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                    left: kDefaultPadding,
-                    right: kDefaultPadding,
-                    bottom: kDefaultPadding + 20),
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Text(
-                      'Hi Ushopy!',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30),
-                    ),
-                    Spacer(),
-                    Image.asset("assets/images/logo.png"),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.8),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: 'Search',
-                            hintStyle: TextStyle(color: kPrimaryColor),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset("assets/icons/search.svg"),
-                    ],
-                  ),
-                ),
-              )
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(),
+          TitleWithMoreButton(
+            text: 'More',
+            press: () {},
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
---
+++ plantapp/lib/widgets/title_with_more_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+import '../widgets/title_with_custom_underline.dart';
+
+class TitleWithMoreButton extends StatelessWidget {
+  final String text;
+  final Function press;
+
+  TitleWithMoreButton({required this.text, required this.press});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TitleWithCustomUnderline('Recommended'),
+          FlatButton(
+            onPressed: () => press,
+            color: kPrimaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
